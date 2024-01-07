e8d85825e58177f4ffd9b5ef05e6c55dc44ac575
Meherbs <meherbensalah4@gmail.com>
2024-01-07 12:51:33 +0100

    v1.0.6

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,7 +1,7 @@
 <script>
     const laraExcelCraftFileImportRoute = "{{ route('lara_excel_craft.file_import') }}";
     const laraExcelCraftTableFetchRoute = "{{ route('lara_excel_craft.table_fetch') }}";
-    const laraExcelCraftFileColumnsRoute = "{{ route('lara_excel_craft.file_columns', ['fileName', '__#FILENAME_lec#__']) }}";
-    const laraExcelCraftFileDataRoute = "{{ route('lara_excel_craft.file_data', ['fileName', '__#FILENAME_lec#__']) }}";
+    const laraExcelCraftFileColumnsRoute = "{{ route('lara_excel_craft.file_columns', ['fileName' => '__#FILENAME_lec#__']) }}";
+    const laraExcelCraftFileDataRoute = "{{ route('lara_excel_craft.file_data', ['fileName' => '__#FILENAME_lec#__']) }}";
     const laraExcelCraftExcelConfirmImportRoute = "{{ route('lara_excel_craft.excel_confirm_import') }}";
 </script>
